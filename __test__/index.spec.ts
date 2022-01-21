42d12bfb1560d3ef56c4981478e0ed2665128536
Dominic Griesel <d.griesel@gmx.net>
2022-01-21 15:43:09 +0100

    fix: add dummy test

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,19 +1,3 @@
-// import test from 'ava'
+import test from "ava";
 
-// import { CRC16_CCITT } from '../index'
-
-// test('input: (empty)', (t) => {
-//   t.is(CRC16_CCITT(Buffer.from([])), 0x1d0f)
-// })
-
-// test('input: A', (t) => {
-//   t.is(CRC16_CCITT(Buffer.from('A', 'ascii')), 0x9479)
-// })
-
-// test('input: 123456789', (t) => {
-//   t.is(CRC16_CCITT(Buffer.from('123456789', 'ascii')), 0xe5cc)
-// })
-
-// test('input: A x 256', (t) => {
-//   t.is(CRC16_CCITT(Buffer.alloc(256, 'A', 'ascii')), 0xe938)
-// })
+test.todo("dummy");
